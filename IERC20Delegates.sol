d0023cd7f15a6c2ee22732314079efd15320bb0d
sherlock-admin4 <162441180+sherlock-admin4@users.noreply.github.com>
2024-11-28 09:26:47 +0100

    message

---
+++ IERC20Delegates.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.23;
+
+import {IERC20} from "openzeppelin/token/ERC20/IERC20.sol";
+import {IDelegates} from "src/interfaces/IDelegates.sol";
+
+/// @notice A subset of the ERC20Votes-style governance token to which a staking token should
+/// conform. Methods related to standard ERC20 functionality and to delegation are included.
+/// These methods are needed in the context of this system. Methods related to checkpointing,
+/// past voting weights, and other functionality are omitted.
+interface IERC20Delegates is IERC20, IDelegates {}
